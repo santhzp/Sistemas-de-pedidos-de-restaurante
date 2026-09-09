333669534f2a9dc2cf248afe30eb8748583ef4f1
Mattheo Santos <smattheo550@gmail.com>
2026-09-09 19:22:10 -0300

    Cardapio (bebidas quentes)

---
+++ cardapio.py
@@ -0,0 +1,12 @@
+print("--------------------")
+print("     CARDÁPIO       ")
+print("--------------------\n")
+
+print("BEBIDAS QUENTES:\n")
+
+print("1 - Expresso único (Forte, Marcante e Intenso)")
+print("2 - Expresso duplo (Dose dupla)")
+print("3 - Cappuccino (Mistura única de leite, café e chocolate)")
+print("4 - Latte (Uma dose de expresso único com delicioso leite vaporizado)")
+
+produto = input("Escolha o produto: ")
