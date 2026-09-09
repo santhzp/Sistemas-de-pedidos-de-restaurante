504ded28b975d51710c7484dc76e1007a9f6aeb6
Mattheo Santos <smattheo550@gmail.com>
2026-09-09 19:33:31 -0300

    cria sistemas de escolha de pedidos

--- cardapio.py
+++ cardapio.py
@@ -4,9 +4,20 @@ print("--------------------\n")
 
 print("BEBIDAS QUENTES:\n")
 
-print("1 - Expresso único (Forte, Marcante e Intenso)")
-print("2 - Expresso duplo (Dose dupla)")
-print("3 - Cappuccino (Mistura única de leite, café e chocolate)")
-print("4 - Latte (Uma dose de expresso único com delicioso leite vaporizado)")
+print("1 - Expresso único (Forte, Marcante e Intenso) - R$7 ")
+print("2 - Expresso duplo (Dose dupla) - R$10 ")
+print("3 - Cappuccino (Mistura única de leite, café e chocolate) - R$14 ")
+print("4 - Latte (Uma dose de expresso único com delicioso leite vaporizado) - R$16 ")
 
 produto = input("Escolha o produto: ")
+
+produto_num = int(produto)
+
+if produto_num == 1: 
+    print("Você escolheu o Expresso único")
+    print("Preço: R$7 ")
+elif produto_num == 2:
+    print("Você escolheu o Expresso Duplo")
+    print("Preço: R$10 ")
+
+
