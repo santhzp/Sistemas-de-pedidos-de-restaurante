c8ce268f5ca8864d2a876ebb2b0463b72fb34b75
Mattheo Santos <smattheo550@gmail.com>
2026-09-13 17:41:32 -0300

    feat: permite comprar mais de um produto

--- cardapio.py
+++ cardapio.py
@@ -1,5 +1,7 @@
 print("--------------------")
+
 print("     CARDÁPIO       ")
+
 print("--------------------\n")
 
 print("BEBIDAS QUENTES:\n")
@@ -9,46 +11,47 @@ print("2 - Expresso duplo (Dose dupla) - R$10 ")
 print("3 - Cappuccino (Mistura única de leite, café e chocolate) - R$14 ")
 print("4 - Latte (Uma dose de expresso único com delicioso leite vaporizado) - R$16 ")
 
-produto = input("Escolha o produto: ")
+add_produto = "S"
+total = 0
 
-produto_num = int(produto)
+while add_produto == "S":
 
-while produto_num not in (1, 2, 3, 4):
-    print("Opção inválida, tente novamente.")
-    novo_produto = input("Escolha o produto: ")
-    produto_num = int(novo_produto)
+    produto = input("Escolha o produto: ")
+    produto_num = int(produto)
 
+    while produto_num not in (1, 2, 3, 4):
+        print("Opção inválida, tente novamente.")
+        novo_produto = input("Escolha o produto: ")
+        produto_num = int(novo_produto)
 
-if produto_num == 1: 
-    preco = 7
-    print("Você escolheu o Expresso único")
-    print("Preço: R$7 ")
-elif produto_num == 2:
-    preco = 10
-    print("Você escolheu o Expresso Duplo")
-    print("Preço: R$10 ")
-elif produto_num == 3:
-    preco = 14
-    print("Você escolheu o Cappuccino")
-    print("Preço: R$14")
-elif produto_num == 4:
-    preco = 16
-    print("Você escolheu o Latte")
-    print("Preço: R$16")  
+    if produto_num == 1:
+        preco = 7
+        print("Você escolheu o Expresso único")
+        print("Preço: R$7 ")
 
-quantidade = input("Quantidade: ")
-quant_num = int(quantidade)
+    elif produto_num == 2:
+        preco = 10
+        print("Você escolheu o Expresso Duplo")
+        print("Preço: R$10 ")
 
-#adicionar outro produto
-add_produto = input("Deseja adicionar outro produto? [S/N]\n").upper()
+    elif produto_num == 3:
+        preco = 14
+        print("Você escolheu o Cappuccino")
+        print("Preço: R$14")
 
-if add_produto == ("S"):
-    print(add_produto)
+    elif produto_num == 4:
+        preco = 16
+        print("Você escolheu o Latte")
+        print("Preço: R$16")
 
+    quantidade = input("Quantidade: ")
+    quant_num = int(quantidade)
 
-
-
+    compra_atual = quant_num * preco
 
 
-total = quant_num * preco
-print("Total: R${} ".format(total))
+    add_produto = input("Deseja adicionar outro produto? [S/N]\n").upper()
+
+    total = total + compra_atual
+
+    print("Total: R${:.2f}".format(total))
